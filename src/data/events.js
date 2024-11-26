a88a7b3ec1957297d273b2e69ae2ecec54ecbd44
mitchellxh <mitchellxh@gmail.com>
2024-11-26 10:35:58 -0500

    update tab title

--- src/data/events.js
+++ src/data/events.js
@@ -1,47 +1,56 @@
 export const events = [
     {
       id: 1,
-      title: 'A Journey into the World of tidyverse (DSDE Workshop)',
+      title: 'A Journey into the World of tidyverse',
       date: '2024-12-02',
       time: '03:00 PM - 04:00 PM',
       location: 'In-person + Zoom',
-      description: 'Data wrangling and plotting in R.',
+      description: '[DSDE] Data wrangling and plotting in R.',
       registrationLink: 'https://tinyurl.com/dsdeworkshop2'
     },
     {
       id: 2,
-      title: 'AI Lightning Talk (DSDE, FDS, ISPS Workshop)',
+      title: 'AI Lightning Talk',
       date: '2024-12-03',
       time: '11:30 AM - 1:00 PM',
       location: 'In-person + Zoom',
-      description: 'Learn about ways AI is being used as a tool in research via a hybrid lightning talk event.',
+      description: '[DSDE, FDS, ISPS] Learn about ways AI is being used as a tool in research via a hybrid lightning talk event.',
       registrationLink: 'https://dissc.yale.edu/events/ai-series/ai-lightning-talk'
     },
     {
       id: 3,
-      title: 'Promptathon: Celebrate Poorvu\'s 10th Anniversary with AI Coffee Talks for Humans',
+      title: 'Promptathon: AI Coffee Talks for Humans',
       date: '2024-12-08',
       time: '10:00 AM - 12:00 PM',
       location: 'In-person',
-      description: 'A community-based approach to creating and improving prompts for generative AI.',
+      description: '[Poorvu] A community-based approach to creating and improving prompts for generative AI.',
       registrationLink: 'https://yaleconnect.yale.edu/poorvufacultyprograms/rsvp_boot?id=2286378'
     },
     {
       id: 4,
-      title: 'Enabling Equitable AI Access with Google Gemini and NET+ (Internet2 Workshop)',
+      title: 'Enabling Equitable AI Access with Google Gemini and NET+',
       date: '2024-12-17',
       time: '02:00 PM - 05:00 PM',
       location: 'Zoom',
-      description: 'Insightful webinar delving into the transformative potential of GenAI in higher education and explore how the NET+ agreement simplifies its adoption.',
-      registrationLink: 'https://events.internet2.edu/822366?_gl=1*hsochb*_ga*MTMyNzE2NjA0MC4xNzMwMTMxNTYx*_ga_GY7HRD6PTP*MTczMjU3MDAxMS4xLjEuMTczMjU3MDAxOS41Mi4wLjA.'
+      description: '[Internet2] Insightful webinar delving into the transformative potential of GenAI in higher education and explore how the NET+ agreement simplifies its adoption.',
+      registrationLink: 'https://events.internet2.edu/822366?_gl=1*hsochb*_ga*MTMyNzE2NjA0MC4xNzMwMTMxNTYx*_ga_GY7HRD6PTP*MTczMjU3MDAxMS4xLjEuMTczMjU3MDAxOS41Mi4wLjA'
     },
     {
       id: 5,
-      title: 'AI Workshop with Seth Stephens-Davidowitz (DSDE Workshop)',
+      title: 'High-Performance Computing and Software Sustainability: Toward Green Software Development',
+      date: '2025-01-15',
+      time: '01:00 PM - 02:00 PM',
+      location: 'Zoom',
+      description: '[IDEAS] With the growth of AI and exascale, systems are requiring 30+ MW, enough to power sizable towns. Discussion on how to produce better software in terms of more energy-efficient approaches.',
+      registrationLink: 'https://ideas-productivity.us16.list-manage.com/track/click?u=5438ff2caf2456f6ec49ebfbf&id=07189986bc&e=9ef6a41391'
+    },
+    {
+      id: 6,
+      title: 'AI Workshop with Seth Stephens-Davidowitz',
       date: '2025-03-28',
       time: '12:00 PM - 1:00 PM',
       location: 'In-person + Zoom',
-      description: 'Workshop with author and economist Seth Stephens-Davidowitz on how to use Chat GPT as an aid for data analyses.',
+      description: '[DSDE] Workshop with author and economist Seth Stephens-Davidowitz on how to use Chat GPT as an aid for data analyses.',
       registrationLink: 'https://dissc.yale.edu/events/ai-series/ai-workshop-seth-stephens-davidowitz'
     }
   ];

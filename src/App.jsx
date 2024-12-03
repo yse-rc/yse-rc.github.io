2c46526e793b5b5f0582823a6954860d0fdd40cd
yse-rc <yse-rc@yale.edu>
2024-12-03 13:57:02 -0600

    Changed routing to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import ProjectBoard from './components/pages/HomePage'
 import { InfoPage } from './components/pages/InfoPage'
 import './styles/index.css'
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<ProjectBoard />} />
-        <Route path="/info" element={<InfoPage />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<ProjectBoard />} />
+          <Route path="/info" element={<InfoPage />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 

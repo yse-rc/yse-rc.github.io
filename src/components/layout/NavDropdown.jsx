959c0776e25051a218a1f491e591acd051bb093f
mitchellxh <mitchellxh@gmail.com>
2025-01-30 16:30:43 -0500

    title font and format

--- src/components/layout/NavDropdown.jsx
+++ src/components/layout/NavDropdown.jsx
@@ -8,7 +8,7 @@ export const NavDropdown = ({ title, options, className }) => {
     <div className={`relative md:inline-block w-full md:w-auto ${className}`}>
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className={`w-full md:w-auto px-4 py-2 text-black hover:text-gray-700 active:text-gray-900 font-normal text-lg bg-gray-50 rounded-lg border border-transparent hover:border-blue-700 active:border-black ${className} flex items-center justify-center md:justify-start gap-2`}
+        className={`w-full md:w-auto px-4 py-2 text-black hover:text-gray-700 active:text-gray-900 font-normal text-lg bg-gray-50 rounded-lg border border-transparent hover:border-blue-700 active:border-black flex items-center justify-center md:justify-start gap-2`}
         onBlur={() => setTimeout(() => setIsOpen(false), 200)}
       >
         {title}
@@ -22,7 +22,7 @@ export const NavDropdown = ({ title, options, className }) => {
         </svg>
       </button>
       {isOpen && (
-        <div className="absolute right-0 md:left-auto mt-1 py-2 bg-gray-50 rounded-lg shadow-lg z-10">
+        <div className="absolute right-0 md:right-auto md:left-0 mt-1 py-2 bg-gray-50 rounded-lg shadow-lg z-10">
           {options.map((option, index) => {
             const isObjectOption = typeof option === 'object';
             const label = isObjectOption ? option.label : option;

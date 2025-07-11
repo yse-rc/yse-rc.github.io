bc20a13ab13878e01c6fe973c2fae1191692df7e
stanleysxtan99 <stanleytansx@gmail.com>
2025-07-11 05:35:09 -0400

    added ml-auto for right

--- src/components/pages/InfoPage.jsx
+++ src/components/pages/InfoPage.jsx
@@ -89,7 +89,7 @@ export const InfoPage = () => {
                   <img
                           src={stanPic2}
                           alt="Stanley Bio Graph"
-                          className="w-[80%] h-auto object-contain mb-6"
+                          className="w-[80%] h-auto object-contain mb-6 ml-auto"
 
 
                         />
